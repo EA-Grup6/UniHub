2032c72d66acfe70e627352e1094712164dba254
kike <47569287+Waizzu@users.noreply.github.com>
2021-04-23 02:59:10 +0200

    delete account funcion added but not working 100%

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,21 +13,36 @@ import { ProfileComponent } from './profile/profile.component';
 export class AppComponent{
 
   title = 'frontend';
+  loginpopup = true;
   isLogged = false;
+  
 
   constructor (private router: Router, public dialog: MatDialog){}
 
   ngOnInit(){
   }
-
-  onLoginClick(){
-    const MatDialogRef = this.dialog.open(RegisterComponent,{
-      width: '100%',
-      height: '100%'
-    });
-    MatDialogRef.afterClosed().subscribe(data=>{
-      console.log(data);
-      this.isLogged = true;
-    });
+  
+  
+    onLoginClick(){
+      if (this.loginpopup)
+      {
+      const MatDialogRef = this.dialog.open(RegisterComponent,{
+        width: '100%',
+        height: '100%'     
+    
+      });
+      MatDialogRef.afterClosed().subscribe(data=>{
+        console.log(data);
+        
+        this.isLogged = true;
+        
+      });
+    }
+    this.loginpopup = false;//con esto evitamos que salgan mas logins que Minions 
   }
+  }
+
+function onLoginClick() {
+  throw new Error('Function not implemented.');
 }
+
