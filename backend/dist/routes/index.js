ee846a581a4020e12e1fda0165953c6d323cb599
roldaninho <72012269+roldaninho@users.noreply.github.com>
2021-04-30 18:33:17 +0200

    Fixing Firebase

--- backend/dist/routes/index.js
+++ backend/dist/routes/index.js
@@ -7,4 +7,5 @@ router.route('/').get(user_controller_1.helloWorld); //la part lògica esta en u
 router.route('/User/newUser/').post(user_controller_1.createUser);
 router.route('/User/loginUser/').post(user_controller_1.loginUser);
 router.route('/User/deleteUser/').post(user_controller_1.deleteUser);
+//router.route('/User/loginUser/').put(loginUser); //Forgot password
 exports.default = router;
