c29f2396523bd0284cb118082dca646c02a0a3e6
pcc1999 <pcasana@protonmail.com>
2021-05-07 02:08:07 +0200

    Preparing Flutter for JWT

--- Unihub_app/lib/main.dart
+++ Unihub_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:unihub_app/app.dart';
+
+//final storage = FlutterSecureStorage();
 
 void main() {
   runApp(App());
