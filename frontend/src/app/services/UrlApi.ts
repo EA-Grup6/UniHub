6f4107b384300c516bc054653db526e36ddaf27d
ajotamm99 <72545812+ajotamm99@users.noreply.github.com>
2021-05-12 12:21:37 +0200

    .

--- frontend/src/app/services/UrlApi.ts
+++ frontend/src/app/services/UrlApi.ts
@@ -2,5 +2,5 @@ export class UrlApi {
     production: false;
     //localhost
     //147.83.7.164
-    urlUser = 'http://unihub_backend_1:4000/User';
+    urlUser = 'http://147.83.7.164:4000/User';
 }
