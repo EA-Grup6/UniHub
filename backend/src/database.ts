41f91bbd03f8be013723df546fd939f5c6f07849
pcc1999 <pcasana@protonmail.com>
2021-05-28 12:00:32 +0200

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -5,7 +5,7 @@ import { isJSDocAugmentsTag } from 'typescript'
 export async function startConnection(){
 //127.0.0.1
 //mongo
-    const db = await mongoose.connect('mongodb://myUserAdmin:abc@unihub_mongo_1:27017/UniHub',{ 
+    const db = await mongoose.connect('mongodb://myUserAdmin:abc123@unihub_mongo_1:27017/UniHub',{ 
         useNewUrlParser: true,
         useFindAndModify: false
     })
