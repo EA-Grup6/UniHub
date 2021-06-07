6d6cb2c8afae8e1ab8a8c6d576c1e61ebaedc6bd
Skry777 <axel.rogui@gmail.com>
2021-06-07 18:58:41 +0200

    avance filtros

--- Unihub_app/lib/screens/forum/filter.dart
+++ Unihub_app/lib/screens/forum/filter.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unihub_app/controllers/editProfile_controller.dart';
+import 'package:unihub_app/models/faculty.dart';
+import 'package:unihub_app/models/degree.dart';
+import 'package:unihub_app/models/university.dart';
+import 'package:unihub_app/models/user.dart';
+import 'package:unihub_app/screens/homepage/homepage.dart';
+import 'package:unihub_app/screens/login/login.dart';
+import 'package:chips_choice/chips_choice.dart';
 import 'package:unihub_app/controllers/offer_controller.dart';
+
+String finalUsername;
+UserApp currentUser;
+List<University> universitiesList = [];
+List<Faculty> schoolsList = [];
+List<Degree> degreesList = [];
+List<String> subjectsList = [];
+List<String> universitiesNamesList = [];
+List<String> schoolsNamesList = [];
+List<String> degreesNamesList = [];
+
+String valueTipo;
+List listTipo = [
+  "Class",
+  "Assignment",
+  "Training for an exam",
+];
 
 class FilterScreen extends StatefulWidget {
   Filter createState() => Filter();
@@ -25,37 +53,20 @@ class Filter extends State<FilterScreen> {
     return prefs.get('username');
   }
 
+  @override
+  void initState() {
+    getValidationData().whenComplete(() async {
+      await getUniversities();
+    });
+    super.initState();
+  }
+
+  String universitySelected;
+  String schoolSelected;
+  String degreeSelected;
+  String subjectsSelected;
+
   String valueAsign;
-  List listAsigns = [
-    "EA",
-    "MF",
-    "AERO",
-    "MV",
-    "ITA",
-    "MGTA",
-    "DSA",
-    "XLAM",
-    "ALGEBRA",
-    "AMPLI1",
-    "AMPLI2",
-    "PDS",
-    "INFO2",
-    "SX",
-    "TIQ",
-    "CSD"
-  ];
-  String valueTipo;
-  List listTipo = [
-    "Class",
-    "Assignment",
-    "Training for an exam",
-  ];
-  String valueUniversity;
-  List listUniversity = [
-    "UPC",
-  ];
-  String valueCollege;
-  List listColleges = ['EETAC', 'EEAAB', 'ETSEIB'];
 
   @override
   Widget build(BuildContext context) {
@@ -95,46 +106,106 @@ class Filter extends State<FilterScreen> {
                                     height: 20,
                                   ),
                                   Container(
-                                      padding:
-                                          EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("Filter by university:"),
-                                          DropdownButton(
-                                            value: valueUniversity,
-                                            onChanged: (newValue) {
-                                              setState(() {
-                                                valueUniversity = newValue;
-                                              });
-                                            },
-                                            items:
-                                                listUniversity.map((valueItem) {
-                                              return DropdownMenuItem(
-                                                value: valueItem,
-                                                child: Text(valueItem),
-                                              );
-                                            }).toList(),
-                                          ),
-                                          Text("Filter by the college:"),
-                                          DropdownButton(
-                                            value: valueCollege,
-                                            onChanged: (newValue) {
-                                              setState(() {
-                                                valueCollege = newValue;
-                                              });
-                                            },
-                                            items:
-                                                listColleges.map((valueItem) {
-                                              return DropdownMenuItem(
-                                                value: valueItem,
-                                                child: Text(valueItem),
-                                              );
-                                            }).toList(),
-                                          ),
-                                        ],
-                                      )),
+                                    child: new DropdownButton<String>(
+                                      isExpanded: true,
+                                      value: universitySelected,
+                                      hint: Text('University'),
+                                      items:
+                                          universitiesNamesList.map((String e) {
+                                        return DropdownMenuItem<String>(
+                                          value: e,
+                                          child: Text(e),
+                                        );
+                                      }).toList(),
+                                      onChanged: (String e) {
+                                        setState(() {
+                                          universitySelected = e;
+                                        });
+                                        getSchools(e);
+                                      },
+                                    ),
+                                  ),
+                                  FutureBuilder<List<String>>(
+                                      future: getSchools(universitySelected),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasData) {
+                                          return Container(
+                                            child: new DropdownButton<String>(
+                                              isExpanded: true,
+                                              value: schoolSelected,
+                                              hint: Text('School'),
+                                              items:
+                                                  snapshot.data.map((String e) {
+                                                return DropdownMenuItem<String>(
+                                                  value: e,
+                                                  child: Text(e),
+                                                );
+                                              }).toList(),
+                                              onChanged: (String e) async {
+                                                setState(() {
+                                                  schoolSelected = e;
+                                                });
+                                              },
+                                            ),
+                                          );
+                                        } else {
+                                          return Container();
+                                        }
+                                      }),
+                                  FutureBuilder<List<String>>(
+                                      future: getDegrees(schoolSelected),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasData) {
+                                          return Container(
+                                            child: new DropdownButton<String>(
+                                              isExpanded: true,
+                                              value: degreeSelected,
+                                              hint: Text('Degree'),
+                                              items:
+                                                  snapshot.data.map((String e) {
+                                                return DropdownMenuItem<String>(
+                                                  value: e,
+                                                  child: Text(e),
+                                                );
+                                              }).toList(),
+                                              onChanged: (String e) async {
+                                                setState(() {
+                                                  degreeSelected = e;
+                                                });
+                                              },
+                                            ),
+                                          );
+                                        } else {
+                                          return Container();
+                                        }
+                                      }),
+                                  FutureBuilder<List<String>>(
+                                      future: getSubjects(degreeSelected),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasData) {
+                                          return Container(
+                                            child: new DropdownButton<String>(
+                                              isExpanded: true,
+                                              value: subjectsSelected,
+                                              hint: Text('Subject'),
+                                              items:
+                                                  snapshot.data.map((String e) {
+                                                return DropdownMenuItem<String>(
+                                                  value: e,
+                                                  child: Text(e),
+                                                );
+                                              }).toList(),
+                                              onChanged: (String e) async {
+                                                setState(() {
+                                                  subjectsSelected = e;
+                                                });
+                                              },
+                                            ),
+                                          );
+                                        } else {
+                                          return Container();
+                                        }
+                                      }),
                                   Container(
                                       padding:
                                           EdgeInsets.fromLTRB(10, 10, 10, 10),
@@ -142,31 +213,7 @@ class Filter extends State<FilterScreen> {
                                         mainAxisAlignment:
                                             MainAxisAlignment.spaceBetween,
                                         children: [
-                                          Text("Filter by the subject:"),
-                                          DropdownButton(
-                                            value: valueAsign,
-                                            onChanged: (newValue) {
-                                              setState(() {
-                                                valueAsign = newValue;
-                                              });
-                                            },
-                                            items: listAsigns.map((valueItem) {
-                                              return DropdownMenuItem(
-                                                value: valueItem,
-                                                child: Text(valueItem),
-                                              );
-                                            }).toList(),
-                                          ),
-                                        ],
-                                      )),
-                                  Container(
-                                      padding:
-                                          EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("Filter by type:"),
+                                          Text("Type:"),
                                           DropdownButton(
                                             value: valueTipo,
                                             onChanged: (newValue) {
@@ -190,12 +237,88 @@ class Filter extends State<FilterScreen> {
                                               Colors.blue),
                                     ),
                                     child: Text(
-                                      'Filter the feed',
+                                      'Filter',
                                       style: TextStyle(
                                           fontSize: 20, color: Colors.white),
                                     ),
                                     onPressed: () async {},
                                   )
                                 ]))))))));
+  }
+
+  Future getValidationData() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var username = preferences.getString('username');
+    setState(() {
+      finalUsername = username;
+    });
+    currentUser = ModalRoute.of(this.context).settings.arguments as UserApp;
+
+    currentUser.university == null
+        ? universitySelected = currentUser.university
+        : universitySelected = 'Not Selected';
+  }
+
+  Future<List<String>> getUniversities() async {
+    universitiesList = [];
+    universitiesNamesList = [];
+    schoolsNamesList = [];
+    degreesList = [];
+    degreesNamesList = [];
+    subjectsList = [];
+    http.Response response = await EditProfileController().getUniversities();
+    for (var university in jsonDecode(response.body)) {
+      universitiesList.add(University.fromMap(university));
+      universitiesNamesList.add(University.fromMap(university).name);
+    }
+    print(universitiesNamesList);
+    universitiesNamesList.add('Not Selected');
+    if (universitySelected != 'Not Selected' || universitySelected != null) {
+      getSchools(universitySelected);
+      if (schoolSelected != null || schoolSelected != 'Not Selected') {
+        getDegrees(schoolSelected);
+        if (degreeSelected != null || degreeSelected != 'Not Selected') {
+          getSubjects(degreeSelected);
+        }
+      }
+    }
+  }
+
+  Future<List<String>> getSchools(String uniName) async {
+    schoolsNamesList = [];
+    degreesList = [];
+    degreesNamesList = [];
+    subjectsList = [];
+    for (var university in universitiesList) {
+      if (university.name == uniName) {
+        schoolsNamesList = new List<String>.from(university.schools);
+      }
+    }
+    print(schoolsNamesList);
+    schoolsNamesList.add('Not Selected');
+    return schoolsNamesList;
+  }
+
+  Future<List<String>> getDegrees(String schoolParam) async {
+    degreesList = [];
+    degreesNamesList = [];
+    subjectsList = [];
+    http.Response response =
+        await EditProfileController().getSchool(schoolParam);
+    Faculty school = Faculty.fromMap(jsonDecode(response.body));
+    degreesNamesList = new List<String>.from(school.degrees);
+    print(degreesNamesList);
+    degreesNamesList.add('Not Selected');
+    return degreesNamesList;
+  }
+
+  Future<List<String>> getSubjects(String degreeParam) async {
+    subjectsList = [];
+    http.Response response =
+        await EditProfileController().getDegree(degreeParam);
+    Degree degree = Degree.fromMap(jsonDecode(response.body));
+    subjectsList = new List<String>.from(degree.subjects);
+    print(subjectsList);
+    return subjectsList;
   }
 }
