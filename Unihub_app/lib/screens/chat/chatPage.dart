754a9ec110c4003c94412b628a9996a515ae29e3
pcc1999 <pcasana@protonmail.com>
2021-07-21 14:26:17 +0200

    Changed chatPage layout

--- Unihub_app/lib/screens/chat/chatPage.dart
+++ Unihub_app/lib/screens/chat/chatPage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
 import 'package:unihub_app/i18N/appTranslations.dart';
 import 'package:unihub_app/models/message.dart';
-import '../../models/user.dart';
 import '../../controllers/chat_controller.dart';
 
 class ChatPage extends StatefulWidget {
@@ -18,80 +16,143 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   final TextEditingController textEditingController = TextEditingController();
 
-  Widget buildSingleMessage(Message message) {
-    return Row(
-        mainAxisAlignment: message.senderID == widget.friend
-            ? MainAxisAlignment.start
-            : MainAxisAlignment.end,
-        children: [
-          Flexible(
-              child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: message.senderID == widget.friend
-                        ? Colors.grey[800]
-                        : Theme.of(context).accentColor,
-                  ),
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
-                  constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width / 1.5),
-                  child: Text(message.text,
-                      style: TextStyle(
-                          fontSize: 15.0,
-                          color: message.senderID == widget.friend
-                              ? Colors.grey[300]
-                              : Theme.of(context).textTheme.headline6.color))))
-        ]);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.friend),
       ),
-      body: Builder(builder: (context) {
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ChatStream(),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            decoration: kMessageContainerDecoration,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Material(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white,
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 4.0, top: 2, bottom: 2, right: 4),
+                      child: TextField(
+                        minLines: 1,
+                        maxLines: 4,
+                        controller: textEditingController,
+                        decoration: kMessageTextFieldDecoration,
+                      ),
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                    shape: CircleBorder(),
+                    color: Colors.blue,
+                    onPressed: () async {
+                      if (textEditingController.text != '') {
+                        widget.chatController.sendMessage(
+                            textEditingController.text, widget.friend);
+                        textEditingController.clear();
+                        setState(() {});
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  var kMessageTextFieldDecoration = InputDecoration(
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    hintText: AppLocalizations.instance.text("message_newMessage", null),
+    hintStyle: TextStyle(color: Colors.grey[500], fontSize: 14.0),
+    border: InputBorder.none,
+  );
+  var kMessageContainerDecoration = BoxDecoration(
+    color: Colors.transparent,
+  );
+
+  Widget ChatStream() {
+    return Builder(
+      builder: (context) {
+        List<SingleMessage> messageWidgets = [];
         List<Message> listMessage =
             widget.chatController.getMessagesForChatID(widget.friend);
-        return Container(
-          child: ListView.builder(
-            itemCount: listMessage.length,
-            itemBuilder: (BuildContext context, int index) {
-              return buildSingleMessage(listMessage[index]);
-            },
+        for (var message in listMessage) {
+          final msgBubble = SingleMessage(message);
+          messageWidgets.add(msgBubble);
+        }
+        return Expanded(
+          child: ListView(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            children: messageWidgets,
           ),
         );
-      }),
-      bottomSheet: TextField(
-        style: TextStyle(color: Colors.grey[800], fontSize: 14.0),
-        controller: textEditingController,
-        decoration: InputDecoration(
-          suffixIcon: IconButton(
-            icon: Icon(Icons.send_rounded,
-                color: this.textEditingController.text == ''
-                    ? Colors.grey
-                    : Colors.blue[400]),
-            onPressed: () async {
-              if (textEditingController.text != '') {
-                widget.chatController
-                    .sendMessage(textEditingController.text, widget.friend);
-                textEditingController.text = '';
-                setState(() {});
-              }
-            },
-          ),
-          hintText: AppLocalizations.instance.text("message_newMessage"),
-          hintStyle: TextStyle(color: Colors.grey[500], fontSize: 14.0),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            borderSide: BorderSide(color: Colors.grey[400], width: 1),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            borderSide: BorderSide(color: Colors.grey[500], width: 1),
-          ),
-        ),
+      },
+    );
+  }
+}
+
+class SingleMessage extends StatelessWidget {
+  final Message message;
+  const SingleMessage(this.message);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+      child: Column(
+        crossAxisAlignment: message.senderID != message.receiverID
+            ? CrossAxisAlignment.end
+            : CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width / 1.5),
+            child: Material(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                topLeft: message.senderID != message.receiverID
+                    ? Radius.circular(40)
+                    : Radius.circular(0),
+                bottomRight: Radius.circular(40),
+                topRight: message.senderID != message.receiverID
+                    ? Radius.circular(0)
+                    : Radius.circular(40),
+              ),
+              color: message.senderID != message.receiverID
+                  ? Colors.blue
+                  : Colors.white,
+              elevation: 5,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Text(
+                  message.text,
+                  style: TextStyle(
+                    color: message.senderID != message.receiverID
+                        ? Colors.white
+                        : Colors.blue,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
